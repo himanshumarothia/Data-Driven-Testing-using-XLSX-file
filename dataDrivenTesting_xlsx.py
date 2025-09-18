b025e952600c873e2ee5a35cc9e93e4a79a6599e
Himanshu Marothia <74856577+himanshumarothia@users.noreply.github.com>
2025-09-18 14:30:33 +0530

    Add files via upload

---
+++ dataDrivenTesting_xlsx.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By 
+from openpyxl import load_workbook
+import time 
+
+#load the excelsheet 
+workbook = load_workbook('Book1.xlsx')
+
+#selecting Active sheet 
+sheet = workbook.active
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+time.sleep(1)
+
+for row in sheet.iter_rows(min_row=2,max_row=sheet.max_row,values_only=True) :
+    username = row[0]
+    password = row[1]
+
+    driver.get("https://www.saucedemo.com/")
+    time.sleep(1)
+    driver.find_element(By.ID,"user-name").send_keys(username)
+    driver.find_element(By.ID,"password").send_keys(password)
+    time.sleep(1)
+    driver.find_element(By.XPATH,"//input[@id='login-button']").click()
+
+
+    time.sleep(1)
+    driver.find_element(By.XPATH,"//button[@id='react-burger-menu-btn']").click()
+
+    time.sleep(1)
+    driver.find_element(By.XPATH,"//a[@id='logout_sidebar_link']").click()
+
+    time.sleep(1)
+    
+driver.quit()
